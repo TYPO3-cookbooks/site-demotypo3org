378a626be7626c73e39da5c3a36b54eea11a2d1e
Fabien Udriot <fabien.udriot@ecodev.ch>
2014-01-05 20:40:01 +0400

    [FEATURE] Add Neos to the landing page
    Resolves #3

--- templates/default/index.php
+++ templates/default/index.php
@@ -6,14 +6,18 @@
 $usageFile = 'usage.html';
 
 // Rediect packages
-foreach (array('introduction', 'bootstrap', 'government') as $package) {
-	if (strpos($_SERVER['REQUEST_URI'], '/' . $package) === 0) {
+foreach (array('introduction', 'bootstrap', 'government', 'neos') as $distribution) {
+	if (strpos($_SERVER['REQUEST_URI'], '/' . $distribution) === 0) {
 		$segment = ltrim($_SERVER['REQUEST_URI'], '/');
 		$parts = explode('/', $segment);
 		$host = array_shift($parts);
 		$path = implode('/', $parts);
 		$path = ltrim($path, '/');
+
 		$location = sprintf('http://%s.typo3cms.demo.typo3.org/%s', $host, $path);
+		if ($distribution == 'neos') {
+		    $location = sprintf('http://%s.demo.typo3.org/%s', $host, $path);
+		}
 		header('Location: ' . $location);
 		die();
 	}
