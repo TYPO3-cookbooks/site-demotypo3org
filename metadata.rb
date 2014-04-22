f4bae38ba02ec3be4562417504fbe1f30c4ab13d
Fabien Udriot <fabien.udriot@ecodev.ch>
2014-04-22 17:48:49 +0200

    [TASK] Upgrade version number

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "fabien.udriot@typo3.org"
 license          "Apache 2.0"
 description      "Installs/Configures site-typo3org-mysql"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.4.0"
+version          "1.4.1"
 
 depends "database"
 depends "mysql"
