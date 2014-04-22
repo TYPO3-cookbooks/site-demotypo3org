5ca99d5bba0f8abac6361d0476e5faa28b91e03a
Fabien Udriot <fabien.udriot@ecodev.ch>
2014-04-22 18:33:54 +0200

    [TASK] Remove obsolete pages from keep-alive script

--- templates/default/keep-alive.sh
+++ templates/default/keep-alive.sh
@@ -8,38 +8,16 @@ then
     mkdir $log
 fi
 
+# configured page
 links=(
     "/"
-    "/about-typo3/history/"
-    "/about-typo3/community/"
-    "/features/"
-    "/customizing-typo3/"
-    "/resources/"
-    "/resources/consultancies/"
-    "/resources/documentation/"
-    "/resources/typo3-association/"
-    "/examples/languages-characters/"
-    "/examples/text/"
-    "/examples/headers/"
-    "/examples/text-and-images/"
-    "/examples/images-with-links/"
-    "/examples/image-groups/"
-    "/examples/image-effects/"
-    "/examples/tables/"
-    "/examples/frames/"
-    "/examples/lists/"
-    "/examples/file-downloads/"
-    "/examples/forms/"
-    "/examples/news/"
-    "/examples/site-map/"
-    "/feedback/"
 )
 for i in "${links[@]}"
 do
-    content=`curl -s -L http://demo.typo3.org$i | grep "Powered by"`
+    content=`curl -s -L http://introduction.cms.demo.typo3.org$i | grep "Powered by"`
     if [ -z "$content" ]; then
         echo "Empty content for $i. Resetting website..."
-        $home/reset-demo.sh
+        $home/introduction.cms.demo.typo3.org.reset.sh
 
         # Log incident
         fileName=keep-alive-incident-`date +"%m-%d-%y-%T"`
