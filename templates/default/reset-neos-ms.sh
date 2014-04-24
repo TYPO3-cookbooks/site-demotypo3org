226dc4369d86e23ca70715b2d9a5b32aba7c29fa
Fabien Udriot <fabien.udriot@ecodev.ch>
2014-04-24 10:35:33 +0200

    [TASK] Add mssing template for neos master reset

---
+++ templates/default/reset-neos-ms.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Security stop
+#echo "Remove me if you can!"
+#exit -1
+
+
+##########
+echo "Downloading distribution if necessary"
+
+# Delete distribution if older than a day for saving bandwidth
+find /tmp -type d -mmin +1440 -name "<%= @distribution_name %>" | head -1 | xargs rm -rf
+
+# Move to the temporary directory
+cd /tmp
+
+# If distribution file does not exist, download it.
+if [ ! -d /tmp/<%= @distribution_name %> ];
+then
+
+    rm -f /tmp/composer.phar
+    curl -s https://getcomposer.org/installer | php
+
+    # Download distribution
+    php /tmp/composer.phar --no-interaction create-project typo3/neos-base-distribution <%= @distribution_name %>
+
+    # Update time of the file
+    touch <%= @distribution_name %>
+fi
+
+##########
+
+mysql -u root -p<%= @password_root %> -e "DROP DATABASE <%= @database %>; CREATE DATABASE <%= @database %>";
+
+##########
+
+echo "Resetting file structure and setting up permission..."
+applicationPath=/var/www/vhosts/<%= @host %>/releases/current
+rm -rf $applicationPath
+cp -r /tmp/<%= @distribution_name %> $applicationPath
+
+##########
+echo "Setting up permission..."
+# Prevent Flow "setfilepermissions" commmand error
+# If directory does not exist a warning is raised.
+mkdir $applicationPath/Web/_Resources
+cd $applicationPath; sudo -u root FLOW_CONTEXT=Production ./flow flow:core:setfilepermissions <%= @user %> www-data www-data
+
+##########
+#echo "Building some cache which will make the installer faster..."
+#curl -s http://neos.demo.typo3.org/setup > /dev/null
+
+# Building cache even deeper...
+#curl -s http://neos.demo.typo3.org/setup/login > /dev/null
+
+
+exit $?
